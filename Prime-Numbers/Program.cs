594cf10a03958da942590c202f3d3b5beda61f6e
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-10-27 22:05:27 -0600

    Add project files.

---
+++ Prime-Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        public static bool Prime(int num)
+        {
+            for (int i = 2; i < Math.Sqrt(num); i++)
+            {
+                return num % i == 0 ? true : false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            do
+            {
+                //Prime(num) == true ? Console.WriteLine(num) : Console.Write("");
+                Prime(num);
+                if (Prime(num) == true) 
+                {
+                    Console.WriteLine(num);
+                }
+                num += 1;
+            } while (num <= 10000);
+
+            Console.WriteLine("Press any key to close the program.");
+            Console.ReadKey();
+        }
+    }
+}
